29ff011d80799b16f8d4076a9087d15511edad2e
seidai-nakagawa <67330787+seidai-nakagawa@users.noreply.github.com>
2020-08-18 19:47:43 +0900

    表示順を並び替えまで済。

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_072828) do
+ActiveRecord::Schema.define(version: 2020_08_18_100145) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.text "video"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
