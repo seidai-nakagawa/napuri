a31bb6425d4d4d72d05dd7c63998bc4576103e82
seidai-nakagawa <67330787+seidai-nakagawa@users.noreply.github.com>
2020-11-03 17:14:08 +0900

    tweets/indexアクションの変更。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    # @tweets = user.tweets
     @tweets = Tweet.includes(:user).order("created_at DESC")
-
-
   end
 end
